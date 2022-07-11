136ead13f7aa0bca79d7174a487c106993bd051e
arqPhenry <navashenrym@gmail.com>
2022-07-11 00:44:28 -0300

    Version estable

---
+++ src/components/styles/vars.js
@@ -0,0 +1,21 @@
+export const blanco = '#FFFFFF';
+export const negro = '#0f1419';
+export const negroAlterno = '#343434';
+
+export const grisClaro = '#f7f9f9';
+export const grisDelimitador = '#C7C7C7';
+
+export const azulPrincipal = '#2d68f5';
+
+export const fuente = '"Inter", sans-serif';
+export const pesoLigero = 300;
+export const pesoMedio = 400;
+export const pesoFuerte = 700;
+export const vsm = '12px';
+export const sm = '14px';
+export const md = '16px';
+export const lg = '18px';
+
+// Dark mode
+export const azulFondoOscuro = '#15202b';
+export const azulFondoClaro = 'rgba(30,39,50)';
